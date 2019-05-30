2d112423fd932ac989d099c8702209dd7e254712
Alex Wisowaty <alexander@wisowaty.com>
2019-05-29 20:58:29 -0400

    seed 1 Jour, with a FL with 3 entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Jour.destroy_all
+
+j = Jour.new
+fl = FutureLog.new
+ent = FutureLogEntry.new
+
+j.title = "My Journal"
+j.future_log = fl
+j.save
+
+ent.content = "paint the house"
+ent.future_log = fl
+ent.save
+
+ent2 = FutureLogEntry.new
+ent2.content = "change oil in car"
+ent2.future_log = fl
+ent2.save
+
+ent3 = FutureLogEntry.new
+ent3.content = "fix the cabinets"
+ent3.future_log = fl
+ent3.save
+
+fl.description = "my future log"
+fl.save
